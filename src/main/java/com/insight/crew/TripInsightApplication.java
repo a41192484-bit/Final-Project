c1307ad0bdf9946d2155faf3e0fc55a80133ac14
CSI <a41192484@gmail.com>
2025-11-26 15:30:39 +0900

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/insight/crew/TripInsightApplication.java
@@ -0,0 +1,13 @@
+package com.insight.crew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripInsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripInsightApplication.class, args);
+	}
+
+}
